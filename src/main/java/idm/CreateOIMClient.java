753e922d491851cd85dcb61452041f548896d997
haska <haska@192.168.1.45>
2021-07-11 14:33:04 +0300

    CreateOIMClient class created.

---
+++ src/main/java/idm/CreateOIMClient.java
@@ -0,0 +1,49 @@
+package idm;
+
+import java.util.Hashtable;
+
+import oracle.iam.platform.OIMClient;
+
+public class CreateOIMClient {
+	public static void main(String[] args) {
+		System.out.println("Client will be created");
+		OIMClient oimClient = createClient();
+		if (oimClient != null) {
+			System.out.println("Client created");
+		}
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static OIMClient createClient() {
+		// Creates OIM client object
+		OIMClient oimClient = new OIMClient();
+
+		// Sets server URL
+		String serverURL = "t3://localhost:14000";
+
+		// Sets username and password
+		String username = "xelsysadm";
+		String password = "********";
+
+		try {
+			// Sets environment variables
+			Hashtable env = new Hashtable();
+			env.put(OIMClient.JAVA_NAMING_FACTORY_INITIAL, "weblogic.jndi.WLInitialContextFactory");
+			env.put(OIMClient.JAVA_NAMING_PROVIDER_URL, serverURL);
+
+			System.setProperty("java.security.auth.login.config", "/authwl.conf");
+			System.setProperty("APPSERVER_TYPE", "wls");
+
+			oimClient = new OIMClient(env);
+			oimClient.login(username, password.toCharArray());
+
+		} catch (Exception e) {
+			oimClient = null;
+			System.out.println("Exception while creating OIMClient: " + e.getMessage());
+		}
+
+		return oimClient;
+
+	}
+
+}
